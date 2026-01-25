a7bd30415af36b971ea09c336efc0a44ad419844
gustavoghislandi <gugasouza3003@gmail.com>
2026-01-25 11:17:38 -0300

    WebView

---
+++ WebView.js
@@ -0,0 +1,11 @@
+// WebView
+
+// É um componente que permite embutir conteúdo da web (HTML, JS, CSS) em apps móveis ou desktop, como dentro de apps Android, iOS ou Electron.
+
+//--
+
+// WebView é um componente pronto.
+
+// É uma visão embutida de um navegador dentro de um app, permitindo que você exiba conteúdo web diretamente no aplicativo. Ele é oferecido como parte de SDKs para Android, iOS, e outras plataformas (como Electron).
+
+// Em resumo, é um componente específico que você usa no seu código, não um conceito abstrato.
