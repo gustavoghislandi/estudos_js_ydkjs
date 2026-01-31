36f4cf8219461c3124b5fe102e2abf0f881d5aa7
gustavoghislandi <gugasouza3003@gmail.com>
2026-01-31 13:02:49 -0300

    Início do Capítulo 3

---
+++ Capítulo_3.js
@@ -0,0 +1,5 @@
+// Chapter 3: Digging to the Roots of JS
+
+// [...]"This chapter should begin to answer some of the "Why?" questions that may be cropping up as you explore JS. However, this material is still not an exhaustive exposition of the language; that's what the rest of the book series is for!"[...]
+
+// Iteration
